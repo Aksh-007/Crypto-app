705b63051a3ed4447024297ddb2043e89b30906e
Aksh-007 <akshaybarapatre54@gmail.com>
2023-02-01 00:00:58 +0530

    added header and routing

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import { Button, HStack } from '@chakra-ui/react';
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+   <HStack
+   p={'4'}
+   shadow={'base'}
+   bgColor={'blackAlpha.900'} 
+   >
+    <Button 
+    variant={'unstyle'}
+    color={'white'}
+    >
+        <Link to='/'>Home</Link>
+    </Button>
+
+    <Button 
+    variant={'unstyle'}
+    color={'white'}
+    >
+        <Link to='/coins'>Coins</Link>
+    </Button>
+
+    <Button 
+    variant={'unstyle'}
+    color={'white'}
+    >
+        <Link to='/exchange'>Exchanges</Link>
+    </Button>
+
+    {/* <Button 
+    variant={'unstyle'}
+    color={'white'}
+    >
+        <Link to='/coins/:id'>CoinsDetails</Link>
+    </Button> */}
+
+   </HStack>
+  )
+}
+
+export default Header;
